2aee963563f3ae61c761fa5e478390dcfc297cf3
andrespato <andrepato@me.com>
2016-12-16 17:12:14 +0000

    phone verify
    Adicionado:
    -> módulo PHPMailer
    -> Envio de sms
    ->code-conf.php
    
    Alterado:
    -> dbWriter devolver o id da nova linha criada na bd
    -> viewport ajusta para 100 por cento em dispositivos moveis

--- assets/js/customjs.js
+++ assets/js/customjs.js
@@ -36,7 +36,7 @@ window.fbAsyncInit = function() {
     function login() {
         FB.login(function(response) {
             if (response.status === 'connected') {
-                alert(JSON.stringify(response,null,4));
+                //alert(JSON.stringify(response,null,4));
                 getInfo(2);
 				
 
@@ -66,9 +66,9 @@ window.fbAsyncInit = function() {
                     document.getElementById('status').innerHTML = JSON.stringify(response,null,4);
                 } else if(arg === 2){ 
                     document.getElementById('titulo-login').innerHTML = 'Bem Vindo/a <b>' + response.name + '</b> !<br/> Grupo Bacalhôa Vinhos de Portugal';
-                    document.getElementById('login-options').innerHTML = JSON.stringify(response,null,4);
+                    //document.getElementById('login-options').innerHTML = JSON.stringify(response,null,4);
                     info(response.id);
-                    document.getElementById("logout-btn").disabled = false;
+                    
                 }
             });
     }
@@ -92,13 +92,13 @@ function checkLoginState() {
             
             document.getElementById('titulo-login').innerHTML = 'Grupo Bacalhôa Vinhos de Portugal';
             
-            document.getElementById("logout-btn").disabled = true;
+            document.getElementById("logout-btn").style.display = "none";
             
         } else {
             console.log('You are not logged into Facebook.');
           
             document.getElementById('titulo-login').innerHTML = 'Grupo Bacalhôa Vinhos de Portugal';
-            document.getElementById("logout-btn").disabled = true;
+            document.getElementById("logout-btn").style.display = "none";
 			
         }
     });
@@ -116,37 +116,29 @@ function emailConn(){
             return 0;
         }
         else {
+            
             document.getElementById('titulo-login').innerHTML = 'Bem Vindo/a !<br/> Grupo Bacalhôa Vinhos de Portugal';
-            document.getElementById('login-options').innerHTML = '<b>Está a um passo de usar o Wifi grátis ! </b>Inserir número de telemóvel para receber o código de confirmação</b></br></br>Telemóvel <input type="tel" id="telemField" /></br><input type="button" value="Enviar Código" onclick="sendConfCode();"/>';
             
             $.ajax({
                     type: 'POST',
                     data: { email : email},
                     url: 'dbWriter.php?type=email',
                     success: function(data) {
-                        alert("Sucesso: "+ data);
+                        document.getElementById('login-options').innerHTML = '<b>Está a um passo de usar o Wifi grátis ! </b>Inserir número de telemóvel para receber o código de confirmação</b></br></br>Telemóvel <input type="tel" id="telemField" /></br><input type="button" value="Enviar Código" onclick="sendConfCode('+data+');"/>';
+                        
+                        //alert("Sucesso: "+ data);
                     },
                     error: function(data){
                         alert('Falha ajax -> '+JSON.stringify(data));
                     }
                 });
-            // GERAR CODIGO E ENVIAR AO UTILIZADOR
-//             $.ajax({
-//                    type: 'POST',
-//                    url: 'wifiauth.php',
-//                    success: function(data) {
-//                        alert("Sucesso: "+ data);
-//                    },
-//                    error: function(data){
-//                        alert('Falha ajax -> '+JSON.stringify(data));
-//                    }
-//                });
         }  
 }
 
  
 function info(usr_id){
     var usr = "/"+usr_id;
+    document.getElementById('login-options').innerHTML = "<img src='loading.gif' alt='loading' height='60' width='80'>";
     FB.api(
         usr,
         'GET',
@@ -154,18 +146,18 @@ function info(usr_id){
         function (response) {
             if (response && !response.error) {
             /* handle the result */
-                
-                
                 //(JSON.stringify(response,null,4));
-                
                 $.ajax({
                 type: 'POST',
                 data: {response},
                 url: 'dbWriter.php?type=fb',
-                //contentType: 'application/json; charset=utf-8',
-                //dataType: 'json',
                 success: function(data) {
-                    alert("Sucesso: "+data);
+                    //alert("Sucesso: "+data);
+                    setTimeout(function(){
+                        document.getElementById('login-options').innerHTML = '<b>Bem vindo/a ao Grupo Bacalhôa Vinhos de Portugal ! Aproveite a visita !</b>';
+                        document.getElementById("logout-btn").style.display = "inline";
+                    }, 2000);
+                    
                 },
                 error: function (request, status, error) {
                     alert("Error: "+request.responseText);
@@ -178,8 +170,76 @@ function info(usr_id){
 }
 
 
-function sendConfCode(){
+function sendConfCode(visitanteID){
     var numTelem = document.getElementById('telemField').value;
-    alert(numTelem);
+    var postData = visitanteID+"-"+numTelem;
     
+    //alert(postData);
+    
+    if( !document.getElementById('telemField').value ) {
+        $('#telemField').css('border', '2px solid red');
+    }
+    else{
+        document.getElementById('login-options').innerHTML = "<img src='loading.gif' alt='loading' height='60' width='80'>";
+        // GERAR CODIGO E ENVIAR AO UTILIZADOR
+         $.ajax({
+                type: 'POST',
+                data: {postdata : postData},
+                url: 'phone-conf.php',
+                success: function(data) {
+                    setTimeout(function(){
+                        if(data != 'E-Mail foi enviado'){
+                            document.getElementById('login-options').innerHTML = data;
+                        }
+                        else {
+                            document.getElementById('login-options').innerHTML = '<b>Está a um passo de usar o Wifi grátis ! </b>Inserir código de confirmação enviado</b></br></br>Código <input type="text" id="confCode" /></br><input type="button" value="Entrar" onclick="checkConfCode('+visitanteID+');"/><div id="confStat"></div>';
+                        }
+                    }, 2000);
+                },
+                error: function(data){
+                    alert('Falha ajax -> '+JSON.stringify(data));
+                }
+            });
+    }
+    
+}
+
+function checkConfCode(visitanteID){
+    document.getElementById('confStat').innerHTML = "<img src='loading.gif' alt='loading' height='20' width='40'>";
+    
+    if( !document.getElementById('confCode').value ) {
+        $('#confCode').css('border', '2px solid red');
+        $('#confCode').css('color', 'red');
+        document.getElementById('confStat').innerHTML = "Código Inválido";
+    }
+    else{
+        var postData = visitanteID+"-"+document.getElementById('confCode').value;
+        $.ajax({
+                    type: 'POST',
+                    data: {postdata : postData},
+                    url: 'code-conf.php',
+                    success: function(data) {
+                        setTimeout(function(){
+                            // TRY < 3 - PROXIMA TENTATIVA
+                            if(data < 3){
+                                $('#confCode').css('color', 'red');
+                                document.getElementById('confStat').innerHTML = "Código Inválido - Tem mais "+(3-data)+" tentativa/s";
+                            }
+                            // 500 - SUCESSO
+                            else if(data == 500){
+                                 $('#login-options').css('color', 'green');
+                                document.getElementById('login-options').innerHTML = "Código Validado - Vai ser redirecionado dentro de momentos";
+                            }
+                            // 400 - INATIVO
+                            else{
+                                $('#confCode').css('color', 'red');
+                                document.getElementById('login-options').innerHTML = "Esgotou o número de tentativas para aceder, volte a aceder para tentar de novo";
+                            }
+                        }, 2000);
+                    },
+                    error: function(data){
+                        alert('Falha ajax -> '+JSON.stringify(data));
+                    }
+                });
+    }
 }
